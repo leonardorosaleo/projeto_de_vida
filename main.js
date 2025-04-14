7dd5cd5459f83a0b39340bba9ee8f78f09aec45b
leonardorosaleo <149621801+leonardorosaleo@users.noreply.github.com>
2025-04-14 11:50:20 -0300

    .

--- main.js
+++ main.js
@@ -35,18 +35,19 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
-        return [dias, horas, minutos, segundos];
+        return [dias,horas,minutos,segundos];
     } else {
         return [0, 0, 0, 0];
     }
 }
+function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
 
-function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[1])[0];
-        document.getElementById("horas" + i).textContent = calculaTempo(tempos[1])[1];
-        document.getElementById("min" + i).textContent = calculaTempo(tempos[1])[2];
-        document.getElementById("seg" + i).textContent = calculaTempo(tempos[1])[3];
+    for (let i=0; i<contadores.length;i++){
+       // contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
 
